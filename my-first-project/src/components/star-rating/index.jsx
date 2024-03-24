da87d57a3c440100a2b02c2c765f7fd1d444502a
Pushpamal Gunasena <pushpamal0012@gmail.com>
2024-03-24 22:16:18 +0530

    add icon package

---
+++ my-first-project/src/components/star-rating/index.jsx
@@ -0,0 +1,11 @@
+function StarRating(noOfStars){
+
+    return <div className="star-rating">
+        {
+            [...Array(noOfStars)].map
+        }
+
+    </div>
+}
+
+export default StarRating;
