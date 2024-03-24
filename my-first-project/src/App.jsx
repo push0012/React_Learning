3a962a0a53d507e84d56227e2bcb621b467417ed
Pushpamal Gunasena <pushpamal0012@gmail.com>
2024-03-24 18:58:49 +0530

    first upload

---
+++ my-first-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Accourdian from './components/accordian'
+import RandomColor from './components/random-color'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      {/*<Accourdian/>*/}
+      {/*<RandomColor />*/}
+
+      
+    </div>
+  )
+}
+
+export default App
