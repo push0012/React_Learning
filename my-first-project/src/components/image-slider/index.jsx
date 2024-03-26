714ad6dba2f24818008294e2e2c855c3e67ff767
push0012 <pushpamal0012@gmail.com>
2024-03-26 14:48:49 +0530

    disable something errors

--- my-first-project/src/components/image-slider/index.jsx
+++ my-first-project/src/components/image-slider/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs';
 function ImageSlider({url, limit = 5, page = 1}){
