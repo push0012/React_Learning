4908b6570796b7af4c83661a9d7ea4fed6e82f7c
Pushpamal Gunasena <pushpamal0012@gmail.com>
2024-04-01 21:58:53 +0530

    QR Code Generator Component

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Accourdian from './components/accordian'
 import ImageSlider from './components/image-slider'
 import LoadMoreData from './components/load-more-data'
+import QRCodeGenerator from './components/qr-code-generator'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 import TreeView from './components/tree-view'
@@ -14,16 +15,27 @@ function App() {
 
   return (
     <div className='App'>
-      {/*<Accourdian/>*/}
-      {/*<RandomColor />*/}
-      {/* <StarRating noOfStars = {10}/> */}
 
-      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"}/>
-       */}
-      
-      {/* <LoadMoreData/> */}
+      {/* 01 Component */}
+      {/* <Accourdian /> */}
 
-      <TreeView  menus={menus}/>
+      {/* 02 Component */}
+      {/* <RandomColor /> */}
+
+      {/* 03 Component */}
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* 04 Component */}
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"} /> */}
+
+      {/* 05 Component */}
+      {/* <LoadMoreData /> */}
+
+      {/* 06 Component */}
+      {/* <TreeView menus={menus} /> */}
+
+      {/* 07 Component */}
+      <QRCodeGenerator/>
     </div>
   )
 }
