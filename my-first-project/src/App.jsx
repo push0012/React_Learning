5028bcc8aa848cdfd14e51af5d33f059171487be
Pushpamal Gunasena <pushpamal0012@gmail.com>
2024-04-01 19:43:11 +0530

    changes

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -3,6 +3,7 @@
 import './App.css'
 import Accourdian from './components/accordian'
 import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 
@@ -15,8 +16,10 @@ function App() {
       {/*<RandomColor />*/}
       {/* <StarRating noOfStars = {10}/> */}
 
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"}/>
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"}/>
+       */}
       
+      <LoadMoreData/>
     </div>
   )
 }
