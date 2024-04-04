21bf1028ff40b59316574ce9f14fa269a1628bca
push0012 <pushpamal0012@gmail.com>
2024-04-04 15:45:06 +0530

    scroll indicator component

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -1,22 +1,20 @@
 /* eslint-disable no-unused-vars */
 
 //import './App.css'
-import Accourdian from './components/accordian'
-import ImageSlider from './components/image-slider'
-import LightDarkMode from './components/light-dark-mode'
-import LoadMoreData from './components/load-more-data'
-import QRCodeGenerator from './components/qr-code-generator'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
-import TreeView from './components/tree-view'
-import menus from './components/tree-view/data'
+import Accourdian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import LightDarkMode from "./components/light-dark-mode";
+import LoadMoreData from "./components/load-more-data";
+import QRCodeGenerator from "./components/qr-code-generator";
+import RandomColor from "./components/random-color";
+import ScrollIndicator from "./components/scroll-indicator";
+import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
-
-
   return (
-    <div className='App'>
-
+    <div className="App">
       {/* 01 Component */}
       {/* <Accourdian /> */}
 
@@ -39,9 +37,12 @@ function App() {
       {/* <QRCodeGenerator/> */}
 
       {/* 08 Component */}
-      <LightDarkMode/>
+      {/* <LightDarkMode/> */}
+
+      {/* 09 Component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
